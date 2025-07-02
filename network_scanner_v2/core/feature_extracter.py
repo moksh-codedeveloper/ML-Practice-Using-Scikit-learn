524b049662da7266c069eaa4df1e20d9aa0ac460
moksh-codedeveloper <maldemoksh437@gmail.com>
2025-07-02 09:30:56 +0530

    Refactor feature extraction and implement model training for Isolation Forest and KMeans

--- network_scanner_v2/core/feature_extracter.py
+++ network_scanner_v2/core/feature_extracter.py
@@ -1,25 +1,16 @@
-import os 
-import json 
-import numpy as np 
+import json
+import pandas as pd
 
-def extract_features_from_json(json_file):
-    features = []
-    if not os.path.exists(json_file):
-        print("JSON file not found.")
+def load_flow_stats_as_dataframe(json_file):
     with open(json_file, 'r') as f:
-        try:
-          logs = json.load(f)
-        except json.JSONDecodeError as e:
-          print('An exception occurred')
-          return []
-    for snapshots in logs:
-        for flow_id, flow_stats in snapshots.items():
-            vec = [
-                flow_stats.get("total_packets", 0),
-                flow_stats.get("avg_packet_size", 0.0),
-                flow_stats.get("flow_duration", 0.0),
-                flow_stats.get("packet_rate", 0.0)
-            ]
-            features.append(vec)
-    return np.array(features)
+        logs = json.load(f)
 
+    rows = []
+    for snapshot in logs:
+        for flow_id, stats in snapshot.items():
+            row = stats.copy()
+            row["flow_id"] = flow_id
+            rows.append(row)
+
+    df = pd.DataFrame(rows)
+    return df
---
+++ network_scanner_v2/train_models.py
@@ -0,0 +1,37 @@
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import IsolationForest 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+import joblib
+from sklearn.cluster import KMeans
+from core.feature_extracter import load_flow_stats_as_dataframe
+
+def train_iso_forest_model():
+    X = load_flow_stats_as_dataframe("./core/logs/flow_stats.json")
+    X.drop(columns=["flow_id", "timestamp", 'protocol'])
+    model = IsolationForest(contamination=0.3, random_state=42)
+    model.fit(X)
+    print("[+] Isolation Forest model trained successfully.")
+    joblib.dump(model, "./ml_engine/models/iso_forest_model.pkl")
+    
+def train_kmeans_model():
+    X = load_flow_stats_as_dataframe("./core/logs/flow_stats.json")
+    model = Pipeline([
+        ("scaler", MinMaxScaler()),
+        ("kmeans", KMeans(n_clusters=3, random_state=42))
+    ])
+    
+    print("[+] Training KMeans model...")
+    model.fit(X)
+    print("[+] KMeans model trained successfully.")
+    joblib.dump(model, "./ml_engine/models/kmeans_model.pkl")
+
+if __name__ == "__main__":
+    train_iso_forest_model()
+    train
+    print("[+] All models trained successfully.")
+    print("[+] Models saved to disk.")
+    print("[+] You can now use these models for anomaly detection and clustering tasks.")
