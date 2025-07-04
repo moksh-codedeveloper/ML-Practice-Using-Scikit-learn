95c2051219d523ad65833c6f5292fb556d164ff6
moksh-codedeveloper <maldemoksh437@gmail.com>
2025-07-04 10:50:21 +0530

    Implement code changes to enhance functionality and improve performance

--- network_scanner_v2/core/feature_extracter.py
+++ network_scanner_v2/core/feature_extracter.py
@@ -30,21 +30,3 @@ def load_csv_as_dataframe(csv_path):
         print(f"[!] Failed to load CSV: {e}")
         return pd.DataFrame()
 
-def convert_flow_stats_json_to_csv(json_path, csv_path):
-    rows = []
-
-    with open(json_path, 'r') as f:
-        data = json.load(f)  # entire list of flow snapshots
-
-    for snapshot in data:
-        for flow_id, stats in snapshot.items():
-            row = stats.copy()
-            row["flow_id"] = flow_id
-            rows.append(row)
-
-    df = pd.DataFrame(rows)
-    df.to_csv(csv_path, index=False)
-    print(f"[+] Converted '{json_path}' → '{csv_path}' with {len(df)} flows")
-
-# Usage
-convert_flow_stats_json_to_csv("./logs/flow_stats.json", "./logs/traffic_monitor.csv")
--- network_scanner_v2/train_models.py
+++ network_scanner_v2/train_models.py
@@ -4,10 +4,10 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.pipeline import Pipeline
 import joblib
 from sklearn.cluster import KMeans
-from core.feature_extracter import load_flow_stats_as_dataframe, load_csv_as_dataframe
-
+from core.feature_extracter import load_csv_as_dataframe
+from sklearn.ensemble import RandomForestRegressor
 def train_iso_forest_model():
-    X = load_flow_stats_as_dataframe("./core/logs/traffic_monitor.csv")
+    X = load_csv_as_dataframe("./core/logs/traffic_monitor.csv")
     X = X.drop(columns=["flow_id", "timestamp", 'protocol'], errors='ignore')
     model = IsolationForest(contamination=0.3, random_state=42)
     model.fit(X)
@@ -26,10 +26,20 @@ def train_kmeans_model():
     print("[+] KMeans model trained successfully.")
     joblib.dump(model, "./ml_engine/models/kmeans_model.pkl")
 
-
+def regressor_model():
+    model = Pipeline([
+        ("scaler", StandardScaler()),
+        ("regressor", RandomForestRegressor(n_estimators=100, random_state=42))
+    ])
+    metrics = load_csv_as_dataframe("./logs/data_log.csv")
+    X = metrics.drop(columns=["ip_src", "ip_dest", "timestamp", "protocol", "app_protocol", "suspicious_keywords", "error"], errors='ignore')
+    Y = metrics[["has_suspicious_keywords", "is_ascii"]] 
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    model.fit(X_train, y_train)
+    print("[+] Regressor model trained successfully.")
+    joblib.dump(model, "./ml_engine/models/regressor_model.pkl")
 
 if __name__ == "__main__":
-    train_kmeans_model()
-    print("[+] All models trained successfully.")
-    print("[+] Models saved to disk.")
-    print("[+] You can now use these models for anomaly detection and clustering tasks.")
+    # train_kmeans_model()
+    train_iso_forest_model()
+    # regressor_model()
